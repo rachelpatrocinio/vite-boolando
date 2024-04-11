bd07040da1d5c60e85237e614cc8ced71eb6ea4e
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-11 14:23:15 +0200

    Added: CardItem.vue on AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,128 +1,129 @@
-<script></script>
+<script>
+import CardItem from './CardItem.vue';
+
+export default {
+    components: {
+        CardItem
+    },
+    data(){
+        return{
+            products: [
+                {
+                "id": 1,
+                "frontImage": "1.webp",
+                "backImage": "1b.webp",
+                "brand": "Levi's",
+                "name": "Relaxed Fit",
+                "price": 29.99,
+                "isInFavorites": true,
+                "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                },
+                {
+                "id": 2,
+                "frontImage": "2.webp",
+                "backImage": "2b.webp",
+                "brand": "Guess",
+                "name": "Roses Tee",
+                "price": 20.99,
+                "isInFavorites": true,
+                "badges": [
+                        {
+                        "type": "discount",
+                        "value": "-30%"
+                        }
+                    ]
+                },
+                {
+                "id": 3,
+                "frontImage": "3.webp",
+                "backImage": "3b.webp",
+                "brand": "Come Zucchero Filato",
+                "name": "Voglia di colori pastello",
+                "price": 129.99,
+                "isInFavorites": false,
+                "badges": [
+                        {
+                        "type": "discount",
+                        "value": "-30%"
+                        }
+                    ]
+                },
+                {
+                "id": 4,
+                "frontImage": "4.webp",
+                "backImage": "4b.webp",
+                "brand": "Levi's",
+                "name": "Tee Unisex",
+                "price": 14.99,
+                "isInFavorites": false,
+                "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                },
+                {
+                "id": 5,
+                "frontImage": "5.webp",
+                "backImage": "5b.webp",
+                "brand": "Maya Deluxe",
+                "name": "Stripe Bodice",
+                "price": 99.99,
+                "isInFavorites": true,
+                "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                },
+                {
+                "id": 6,
+                "frontImage": "6.webp",
+                "backImage": "6b.webp",
+                "brand": "Esprit",
+                "name": "Maglione - Black",
+                "price": 29.99,
+                "isInFavorites": true,
+                "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
+</script>
 
 <template>
 <main class="main-content">
     <section>
         <div class="container">
             <div class="row d-flex flex-wrap">
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/1.webp" alt="RELAXED FIT TEE UNISEX">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="badges">
-                                <span class="badge__red">-50%</span>
-                                <span class="badge__green">Sostenibilità</span>
-                            </div>
-                            <div class="overlay">
-                                <img src="/public/1b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div>                
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Levi's</span>
-                            <h5>RELAXED FIT TEE UNISEX</h5>
-                            <span class="price-tag">14,99 €</span>
-                            <span class="price-tag-line-through">29,99 €</span>
-                        </div>
-                    </div>
+                <div class="col-4" v-for="(product,i) in products" :key="product.id">
+                    <CardItem :item="product"/>
                 </div> 
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/2.webp" alt="ROSES TEE">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="badges">
-                                <span class="badge__red">-30%</span>
-                            </div>
-                            <div class="overlay">
-                                <img src="/public/2b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div> 
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Guess</span>
-                            <h5>ROSES TEE</h5>
-                            <span class="price-tag">20,99 €</span>
-                            <span class="price-tag-line-through">29,99 €</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/3.webp" alt="VOGLIA DI COLORI PASTELLO">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="badges">
-                                <span class="badge__red">-30%</span>
-                            </div>
-                            <div class="overlay">
-                                <img src="/public/3b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div>  
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Come Zucchero Filato</span>
-                            <h5>VOGLIA DI COLORI PASTELLO</h5>
-                            <span class="price-tag">129,99 €</span>
-                            <span class="price-tag-line-through">184,99 €</span>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/4.webp" alt="TEE UNISEX">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="badges">
-                                <span class="badge__red">-50%</span>
-                                <span class="badge__green">Sostenibilità</span>
-                            </div>
-                            <div class="overlay">
-                                <img src="/public/4b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div> 
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Levi's</span>
-                            <h5>TEE UNISEX</h5>
-                            <span class="price-tag">14,99 €</span>
-                            <span class="price-tag-line-through">29,99 €</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/5.webp" alt="STRIPE BODICE">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="overlay">
-                                <img src="/public/5b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div> 
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Maya Deluxe</span>
-                            <h5>STRIPE BODICE</h5>
-                            <span class="price-tag">99,99 €</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-4">
-                    <div class="card">
-                        <div class="card__header">
-                            <img src="/public/6.webp" alt="MAGLIONE - BLACK">
-                            <span class="wishlist-btn"> &hearts;</span>
-                            <div class="badges">
-                                <span class="badge__green">Sostenibilità</span>
-                            </div> 
-                            <div class="overlay">
-                                <img src="/public/6b.webp" alt="RELAXED FIT TEE UNISEX 2">
-                            </div> 
-                        </div>
-                        <div class="card__body">
-                            <span class="brand-name">Esprit</span>
-                            <h5>MAGLIONE - BLACK</h5>
-                            <span class="price-tag">29,99 €</span>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
@@ -130,82 +131,4 @@
 </template>
 
 <style lang="scss" scoped>
-/* CARDS */
-
-.card__header{
-    position: relative;
-    img{
-        display: block;
-    }
-}
-
-.brand-name{
-    font-size: 12px;
-}
-
-.wishlist-btn{
-    width: 30px;
-    padding: 3px;
-    font-size: 12px;
-    text-align: center;
-    background-color: white;
-    position: absolute;
-    right: 0;
-    top: 5px;
-    z-index: 100;
-    &:hover{
-        color: red;
-        cursor: pointer;
-    }
-}
-
-.badges{
-    position: absolute;
-    bottom: 25px;
-}
-
-span[class^=badge]{
-    color: white;
-    padding: 3px;
-    font-size: 10px;
-    font-weight: 700;
-    text-align: center;
-    display: inline-block;
-    z-index: 100;
-}
-
-.badge__red{
-    background-color: red;
-    width: 50px;
-}
-
-.badge__green{
-    background-color: green;
-    width: 80px;
-}
-
-.price-tag{
-    color: red;
-    font-weight: 700;
-}
-
-.price-tag-line-through{
-    text-decoration: line-through;
-}
-
-.overlay{
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    opacity: 0;
-}
-
-.card__header:hover .overlay{
-    opacity: 1;
-    transition: 500ms ease-in-out;
-}
-
-.card__header:hover .badges{
-    z-index: 100;
-}
 </style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,110 @@
+<script>
+export default {
+    props:{
+        item:{
+            type: Object
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card__header">
+            <img :src="item.frontImage" alt="RELAXED FIT TEE UNISEX">
+            <span class="wishlist-btn"> &hearts;</span>
+            <div class="badges">
+                <!-- <span class="badge__red"></span>
+                <span class="badge__green"></span> -->
+            </div>
+            <div class="overlay">
+                <img :src="item.backImage" alt="RELAXED FIT TEE UNISEX 2">
+            </div>                
+        </div>
+        <div class="card__body">
+            <span class="brand-name">Levi's</span>
+            <h5>{{item.name}}</h5>
+            <span class="price-tag">{{item.price}} €</span>
+            <span class="price-tag-line-through"></span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* CARDS */
+
+.card__header{
+    position: relative;
+    img{
+        display: block;
+    }
+}
+
+.brand-name{
+    font-size: 12px;
+}
+
+.wishlist-btn{
+    width: 30px;
+    padding: 3px;
+    font-size: 12px;
+    text-align: center;
+    background-color: white;
+    position: absolute;
+    right: 0;
+    top: 5px;
+    z-index: 100;
+    &:hover{
+        color: red;
+        cursor: pointer;
+    }
+}
+
+.badges{
+    position: absolute;
+    bottom: 25px;
+}
+
+span[class^=badge]{
+    color: white;
+    padding: 3px;
+    font-size: 10px;
+    font-weight: 700;
+    text-align: center;
+    display: inline-block;
+    z-index: 100;
+}
+.badge__red{
+    background-color: red;
+    width: 50px;
+}
+
+.badge__green{
+    background-color: green;
+    width: 80px;
+}
+
+.price-tag{
+    color: red;
+    font-weight: 700;
+}
+
+.price-tag-line-through{
+    text-decoration: line-through;
+}
+
+.overlay{
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    opacity: 0;
+}
+
+.card__header:hover .overlay{
+    opacity: 1;
+    transition: 500ms ease-in-out;
+}
+
+.card__header:hover .badges{
+    z-index: 100;
+}</style>
